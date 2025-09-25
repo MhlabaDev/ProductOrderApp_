ae39154915a96a30d43bdc80cb743720008ece55
MhlabaDev <hlabisto28@gmail.com>
2025-09-25 21:55:22 +0200

    Customer change

---
+++ ProductOrderApi/Migrations/20250925195225_UpdatingCustomer.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProductOrderApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdatingCustomer : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Customers",
+                newName: "CustomerId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "CustomerId",
+                table: "Customers",
+                newName: "Id");
+        }
+    }
+}
